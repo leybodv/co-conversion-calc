2210ce3cdaa23ebdf8189b274f8636bbd4aa9435
leybodv <leybodv@gmail.com>
2021-04-26 23:47:09 +0300

    added find_conversion and find_CO2_concentration functions

--- calculate-conversion.py
+++ calculate-conversion.py
@@ -3,11 +3,15 @@
 import sys
 from pathlib import Path
 
+flowRateHe = 30
+initFlowRateCO = 2
+initFlowRateO2 = 4
+
 def print_usage():
     print('Usage: calculate-conversion.py <directory with spectra> <calibration file>')
 
 def parse_spectrum(filepath):
-    print('processing file:', file.resolve())
+    print('processing file:', filepath.resolve())
     with filepath.open() as filehandler:
         for line in filehandler:
             if 'Time	Time Relative [s]	T [°C]	Time	Time Relative [s]	Pressure [mbar]' in line:
@@ -28,6 +32,29 @@ def parse_spectrum(filepath):
                 break
     return [temperature, m, ic_rel]
 
+def find_const_baseline(i):
+    print('function find_const_baseline is not finished!')
+    return 0.00001
+
+def find_CO2_concentration(m, icRel, mCal, icRelCal, flowRateHe, initFlowRateCO, initFlowRateO2):
+    iBaseCal = find_const_baseline(icRelCal)
+    iCO2Cal = icRelCal[mCal.index(44)]
+    iBase = find_const_baseline(icRel)
+    iCO2 = icRel[m.index(44)]
+    xCO2Cal = initFlowRateCO / sum([initFlowRateCO, initFlowRateO2, flowRateHe]) # assumption: calibration was done with the same flow rates as catalytic activity measurement experiment
+    xCO2 = xCO2Cal * (iCO2 - iBase) / (iCO2Cal - iBaseCal)
+    print('xCO2 = ', xCO2)
+    return xCO2
+
+def find_conversion(flowRateHe, initFlowRateCO, initFlowRateO2, specdata, caldata):
+    conversionData = list()
+    for spectrum in specdata:
+        CO2_concentration = find_CO2_concentration(m = spectrum[1], icRel = spectrum[2], mCal = caldata[1], icRelCal = caldata[2], flowRateHe = flowRateHe, initFlowRateCO = initFlowRateCO, initFlowRateO2 = initFlowRateO2)
+        totalInitFlowRate= sum([flowRateHe, initFlowRateCO, initFlowRateO2])
+        conversion = (CO2_concentration * totalInitFlowRate / initFlowRateCO) * (1 / (1 + CO2_concentration / 2))
+        conversionData.append([spectrum[0], conversion])
+    return conversionData
+
 try:
     specdir = Path(sys.argv[1])
     calfile = Path(sys.argv[2])
@@ -43,6 +70,8 @@ if specdir.is_dir() and calfile.is_file():
             specdata.append(parse_spectrum(file))
     print('parsed data length:', len(specdata))
     caldata = parse_spectrum(calfile)
+    conversion = find_conversion(flowRateHe, initFlowRateCO, initFlowRateO2, specdata, caldata)
+    print('conversion:\n', conversion)
 else:
     print_usage()
     quit()
